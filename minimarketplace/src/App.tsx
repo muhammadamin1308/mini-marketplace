954dfa275287163743b44a470bf8428811520664
Muhammadamin <sharifjanovmukhammadamin@gmail.com>
2025-12-15 13:10:24 +0500

    feat: fetch and display products

--- minimarketplace/src/App.tsx
+++ minimarketplace/src/App.tsx
@@ -1,29 +1,24 @@
+import { useEffect } from "react";
 import "./App.css";
 import Cart from "./pages/Cart";
-import { useEffect } from "react";
-// import Home from "./pages/Home";
 
 function App() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "src/pages/Products.ts";
-    document.body.appendChild(script);
-
-    return () => {
-      if (document.body.contains(script)) {
-        document.body.removeChild(script);
-      }
-    };
+    // Dynamic import to avoid Vite processing issues
+    import("./pages/Products.ts");
   }, []);
 
   return (
     <div className="app">
       <header>
-          <h2>Click - Mini Marketplace</h2>
+        <h2>Click - Mini Marketplace</h2>
       </header>
-      <main>
+      <main className="main-container">
         <section className="products-section">
           <h1>Products</h1>
+          <div id='products-container' className="products-container">
+            <p className="loading">Loading Products...</p>
+          </div>
         </section>
         <section className="cart-section">
           <Cart />
---
+++ minimarketplace/src/pages/Products.ts
@@ -0,0 +1,46 @@
+import type { Product } from "../types/index.ts";
+
+function products() {
+  const productsCont = document.getElementById("products-container");
+
+  async function fetchProducts() {
+    try {
+      if (!productsCont) return;
+      productsCont.innerHTML = '<p class="loading">Loading products...</p>';
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      renderProducts(products);
+    } catch (error) {
+      if (!productsCont) return;
+      productsCont.innerHTML = '<p class="error">Failed to load products.</p>';
+      console.error(`Can't fetch products`, error);
+    }
+  }
+
+  function renderProducts(products: Product[]) {
+    if (!productsCont) return;
+    productsCont.innerHTML = products
+      .map(
+        (product) => `
+        <div class="product-card">
+          <img src="${product.image}" alt="${product.title}" />
+          <h3>${product.title}</h3>
+          <p class="description">${product.description}</p>
+          <div class="product-footer">
+            <p class="price">$${product.price}</p>
+            <button class="add-to-cart" data-id="${product.id}">Add to Cart</button>
+          </div>
+        </div>
+      `
+      )
+      .join("");
+  }
+
+  if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", fetchProducts);
+  } else {
+    fetchProducts();
+  }
+}
+
+products();
